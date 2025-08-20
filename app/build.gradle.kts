30133f744c8abb0322aa79551450409c7069731e
CHRISTIAN CUSTODIO <c.custodio@lcc-daet.edu.ph>
2025-08-20 13:58:37 +0800

    chore: update to latest stable sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.leveluplccd"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.leveluplccd"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
